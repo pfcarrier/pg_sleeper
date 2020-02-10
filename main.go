aaee7eb901aae433f455a467947b4852dc123e4f
Pierre Fortin Carrier <pierre.fortincarrier@gmail.com>
2020-02-10 11:52:00 -0500

    add mvp level feature

--- main.go
+++ main.go
@@ -4,25 +4,79 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/jackc/pgx/v4"
 )
 
-func main() {
+var sleeperProbes map[string]int
 
+func init() {
+	sleeperProbes = map[string]int{
+		"1min":   60,
+		"2min":   120,
+		"4min":   240,
+		"5min":   300,
+		"15min":  900,
+		"30min":  1800,
+		"1hour":  3600,
+		"2hour":  7200,
+		"4hour":  14400,
+		"8hour":  28000,
+		"16hour": 57600,
+		"1day":   86400,
+		"2day":   172800,
+		"4day":   345600,
+		"1week":  604800,
+		"2week":  1209600,
+		"3week":  1814400,
+		"4week":  2419200,
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for probeName, probeSec := range sleeperProbes {
+		wg.Add(1)
+		go sleeper(probeName, probeSec, &wg)
+	}
+	fmt.Println("establishing connections...done")
+	wg.Wait()
+}
+
+func getTime() string {
+	return time.Now().Format("2006-01-02 03:04:05")
+}
+
+func sleeper(probeName string, probeSec int, wg *sync.WaitGroup) {
+	var test int64
+	var err error
+	defer wg.Done()
+
+	conn := pgConnect(probeName)
+	defer conn.Close(context.Background())
+
+	for {
+		time.Sleep(time.Second * time.Duration(probeSec))
+		err = conn.QueryRow(context.Background(), "select 1 as test").Scan(&test)
+		if err != nil {
+			//fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			fmt.Println(getTime(), probeName, "FAILURE", "(0.0001s)", err)
+		} else {
+			fmt.Println(getTime(), probeName, "OK", "(0.0001s)")
+		}
+	}
+}
+
+func pgConnect(probeName string) *pgx.Conn {
 	conn, err := pgx.Connect(context.Background(), os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connection to database: %v\n", err)
 		os.Exit(1)
+	} else {
+		//fmt.Println("Sucessfully open connection to database for ", probeName)
 	}
-	defer conn.Close(context.Background())
-
-	var test int64
-	err = conn.QueryRow(context.Background(), "select 1 as test").Scan(&test)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(test)
+	return conn
 }
